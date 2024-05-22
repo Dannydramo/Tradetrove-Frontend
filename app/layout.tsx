fb3dc1455200c83e56c936aeacc81dfce32e1972
Dannydramo <alumandudaniel217@gmail.com>
2024-05-22 04:21:42 -0700

    feat: adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,24 +6,33 @@ import { Toaster } from 'sonner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    metadataBase: new URL(
-        'https://tradetrove-lk33sewi4-dannydramos-projects.vercel.app'
-    ),
+    metadataBase: new URL('https://tradetrove.vercel.app'),
     title: 'Tradetrove',
     description:
         'Welcome to Tradetrove - Your trusted marketplace for unique and valuable items.',
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Tradetrove',
+        description:
+            'Welcome to Tradetrove - Your trusted marketplace for unique and valuable items.',
+        images: ['https://tradetrove.vercel.app/og-image.png'],
+    },
     openGraph: {
         title: 'Tradetrove',
         description:
             'Welcome to Tradetrove - Your trusted marketplace for unique and valuable items.',
+        url: 'https://tradetrove.vercel.app',
+        siteName: 'Tradetrove',
         images: [
             {
-                url: '/og-image.png',
+                url: 'https://tradetrove.vercel.app/og-image.png',
                 width: 800,
                 height: 600,
                 alt: 'Tradetrove - Unique and valuable items',
             },
         ],
+        locale: 'en_US',
+        type: 'website',
     },
 };
 
